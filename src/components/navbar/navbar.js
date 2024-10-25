e6ccf855afc0e7d4bdfa8cc2d54a3789de14c549
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-10-24 21:44:31 -0600

    Navbar styling

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,9 +1,26 @@
 import React from 'react';
+//css
 import './navbar.css';
+//images
+import logo from '../../assets/logo.png'
+import contactImg from '../../assets/contact.png'
+//packages
+import { Link } from 'react-scroll'
 
 const Navbar = () => {
   return (
-    <div>navbar</div>
+    <nav className='navbar'>
+      <img src={logo} alt="Logo" className='logo'/>
+      <div className='desktopMenu'>
+        <Link className='desktopMenuListItem'>Home</Link>
+        <Link className='desktopMenuListItem'>Clients/Skills</Link>
+        <Link className='desktopMenuListItem'>About</Link>
+        <Link className='desktopMenuListItem'>Portfolio</Link>
+      </div>
+      <button className='desktopMenuBtn'>
+        <img src={contactImg} alt="Contact Button" className="desktopMenuImg"/>Contact Me
+      </button>
+    </nav>
   );
 }
 
