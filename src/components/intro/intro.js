272bf9a3785b1648b1010777d1ffcfcc232845d1
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-10-27 17:09:25 -0600

    Intro css class name adjustments

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -7,21 +7,17 @@ import btnImg from '../../assets/hireme.png'
 const Intro = () => {
   return (
     <section className="top" id="intro">
-      <div className="introContent">
+      <div className="intro-content">
         <span className="hello">Howdy!</span>
-        <span className="introText">I'm <br/><span className="introName">Isaac Mitchell</span><br/>Software Engineer</span>
-        {/* <p className="introPara"> I'm a <span className='extra'>beast</span>, dedicated to crafting seamless user experiences <br />and writing clean, test-driven code</p> */}
-        <p className="introPara">I'm a lighthearted and driven engineer who loves building tools that help people. I bring creativity,teamwork, and passion to every project Take a look around!</p>
+        <span className="intro-text">I'm <br/><span className="intro-name">Isaac Mitchell</span><br/>Software Engineer</span>
+        <p className="intro-para">I'm a lighthearted and driven engineer who loves building tools that help people. I bring creativity,teamwork, and passion to every project Take a look around!</p>
         <button className='btn' onClick={() => {
           document.getElementById('contact').scrollIntoView({behavior: 'smooth'})
         }}>
-          <img src={btnImg} alt="Contact Button" className="btnImg"/>Hire Me
+          <img src={btnImg} alt="Contact Button" className="btn-img"/>Hire Me
         </button>
-    
       </div>
-      {/* <div> */}
         <img src={bg} alt="bg" className="bg"/>
-      {/* </div> */}
     </section>
   );
 }
