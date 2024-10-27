0f1d8ecc9bfc789795d3e9ef182f504c5c6df4a4
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-10-27 16:48:01 -0600

    Mobile text for skill cards and professionalized it

--- src/components/skills/skills.js
+++ src/components/skills/skills.js
@@ -8,27 +8,27 @@ const Skills = () => {
   return (
     <section  id="skills">
       <h2 className="skill-title">What I Do</h2>
-      <span className="skill-descr">Experienced software engineer with 8+ years in quality assurance and a foundation in biology and chemistry. Skilled in Angular, TypeScript, Ruby on Rails, SQL, REST APIs, and Playwright, with a focus on clean, scalable code and best practices. Strong communicator, adept at explaining technical concepts and collaborating across teams to deliver business solutions. Proficient in building responsive, user-friendly front-end interfaces.</span>
+      <span className="skill-descr"></span>
       <span className="skill-bars">
         <div className="skill-bar">
           <img src={BackEnd} alt="Back End" className="skill-bar-img"/>
           <div className="skill-bar-text">
             <h2>Back End Development</h2>
-            <p>Write additional informative text right here in the block!! Go ahead try it</p>
+            <p>Skilled in Ruby on Rails, REST APIs, and database management with SQL. Currently diving into Python.</p>
           </div>
         </div>
         <div className="skill-bar">
           <img src={FrontEnd} alt="Front End" className="skill-bar-img"/>
           <div className="skill-bar-text">
             <h2>Front End Development</h2>
-            <p>Some content some content some content</p>
+            <p>Proficient in Angular and TypeScript, with a focus on responsive, user-friendly interfaces while refactoring old code to meet new standards.</p>
           </div>
         </div>
         <div className="skill-bar">
           <img src={Testing} alt="Testing" className="skill-bar-img"/>
           <div className="skill-bar-text">
             <h2>Testing</h2>
-            <p>Write additional informative text right here in the block!! Go ahead try it</p>
+            <p>Skilled in E2E, unit, and integration testing using the Page Object Model (POM) and tools like Playwright to build robust, maintainable applications.</p>
           </div>
         </div>
       </span>
