7e26e4fd0b22591e53dd7cda6af39dc098ae069b
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-10-26 18:47:04 -0600

    Added hamburger menu and styling, navbar overflow to default

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 //css
 import './navbar.css';
 //images
 import logo from '../../assets/isaac-mitchell-high-resolution-logo-transparent.png'
 import contactImg from '../../assets/contact.png'
+import menu from '../../assets/burger-menu.png'
 //packages
 import { Link } from 'react-scroll'
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className='navbar'>
       <img src={logo} alt="Logo" className='logo'/>
       <div className='desktopMenu'>
-        <Link className='desktopMenuListItem'>Home</Link>
-        <Link className='desktopMenuListItem'>Clients/Skills</Link>
-        <Link className='desktopMenuListItem'>About</Link>
-        <Link className='desktopMenuListItem'>Portfolio</Link>
+        <Link activeClass="active" to="intro" spy={true} smooth={true} offset={-100} duration={500} className='desktopMenuListItem'>Home</Link>
+        <Link activeClass="active" to="skills" spy={true} smooth={true} offset={-50} duration={500} className='desktopMenuListItem'>Skills</Link>
+        <Link activeClass="active" to="projects" spy={true} smooth={true} offset={-75} duration={500} className='desktopMenuListItem'>Projects</Link>
+        <Link activeClass="active" to="contact" spy={true} smooth={true} offset={-250} duration={500} className='desktopMenuListItem'>Resume</Link>
       </div>
-      <button className='desktopMenuBtn'>
+      <button className='desktopMenuBtn' onClick={() => {
+        document.getElementById('contact').scrollIntoView({behavior: 'smooth'})
+      }}>
         <img src={contactImg} alt="Contact Button" className="desktopMenuImg"/>Contact Me
       </button>
+
+
+      <img src={menu} alt="Menu" className="ham-menu" onClick={() => setShowMenu(!showMenu)}/>
+      <div className="nav-menu" style={{display: showMenu? 'flex':'none'}}>
+        <Link to="intro" spy={true} smooth={true} offset={-100} duration={500} onClick={() => setShowMenu(false)} className='ham-list-menu'>Home</Link>
+        <Link to="skills" spy={true} smooth={true} offset={-50} duration={500} onClick={() => setShowMenu(false)} className='ham-list-menu'>Skills</Link>
+        <Link to="projects" spy={true} smooth={true} offset={-75} duration={500} onClick={() => setShowMenu(false)} className='ham-list-menu'>Projects</Link>
+        <Link to="contact" spy={true} smooth={true} offset={-250} duration={500} onClick={() => setShowMenu(false)} className='ham-list-menu'>Resume</Link>
+        <Link to="contact" spy={true} smooth={true} offset={-250} duration={500} onClick={() => setShowMenu(false)} className='ham-list-menu'>Contact</Link>
+      </div>
     </nav>
-  );
+  ); 
 }
 
 export default Navbar;
