fca91b33c50916f73878e1e5fea3cfe669079b4e
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-10-27 16:46:37 -0600

    Css changes to make it mobile friendly with the big image and white text overlap

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -9,13 +9,19 @@ const Intro = () => {
     <section className="top" id="intro">
       <div className="introContent">
         <span className="hello">Howdy!</span>
-        <span className="introText">I am <span className="introName">Isaac Mitchell</span><br/>Software Engineer</span>
-        <p className="introPara"> I'm a <span className='extra'>beast</span>, dedicated to crafting seamless user experiences <br />and writing clean, test-driven code</p>
-        <Link><button className="btn"> <img src={btnImg} alt="buttonImage" className="btnImg"/>Hire Me</button></Link>
+        <span className="introText">I'm <br/><span className="introName">Isaac Mitchell</span><br/>Software Engineer</span>
+        {/* <p className="introPara"> I'm a <span className='extra'>beast</span>, dedicated to crafting seamless user experiences <br />and writing clean, test-driven code</p> */}
+        <p className="introPara">I'm a lighthearted and driven engineer who loves building tools that help people. I bring creativity,teamwork, and passion to every project Take a look around!</p>
+        <button className='btn' onClick={() => {
+          document.getElementById('contact').scrollIntoView({behavior: 'smooth'})
+        }}>
+          <img src={btnImg} alt="Contact Button" className="btnImg"/>Hire Me
+        </button>
+    
       </div>
-      <div>
+      {/* <div> */}
         <img src={bg} alt="bg" className="bg"/>
-      </div>
+      {/* </div> */}
     </section>
   );
 }
