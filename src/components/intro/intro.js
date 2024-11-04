c50aed09a368abf784165e0e775a1c9176d1792e
Isaac Mitchell <tmitchellisaac@gmail.com>
2024-11-04 08:14:06 -0700

    Fix period

--- src/components/intro/intro.js
+++ src/components/intro/intro.js
@@ -9,7 +9,7 @@ const Intro = () => {
       <div className="intro-content">
         <span className="hello">Howdy!</span>
         <span className="intro-text">I'm <br/><span className="intro-name">Isaac Mitchell</span><br/>Software Engineer</span>
-        <p className="intro-para">I'm a lighthearted and driven engineer who loves building tools that help people. I bring creativity, teamwork, and passion to every project Take a look around!</p>
+        <p className="intro-para">I'm a lighthearted and driven engineer who loves building tools that help people. I bring creativity, teamwork, and passion to every project. Take a look around!</p>
         <button className='btn' onClick={() => {
           document.getElementById('contact').scrollIntoView({behavior: 'smooth'})
         }}>
